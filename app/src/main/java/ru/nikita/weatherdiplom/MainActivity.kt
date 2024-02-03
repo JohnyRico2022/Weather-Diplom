122c5e4378b5983806f9ce14691a85dc3d61ff06
Nikita Konstantinov <110055310+JohnyRico2022@users.noreply.github.com>
2024-02-03 17:42:44 +0300

    added bottom menu and fragment

--- app/src/main/java/ru/nikita/weatherdiplom/MainActivity.kt
+++ app/src/main/java/ru/nikita/weatherdiplom/MainActivity.kt
@@ -2,10 +2,23 @@ package ru.nikita.weatherdiplom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import ru.nikita.weatherdiplom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.frame_layout)
+
+        binding.bottomNavigation.setupWithNavController(navController)
+
+
     }
 }
