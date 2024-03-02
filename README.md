ec86970d079d00b3355f4aa2dc5e72719791f7da
Nikita Konstantinov <110055310+JohnyRico2022@users.noreply.github.com>
2024-03-02 17:11:50 +0300

    added README

---
+++ README.md
@@ -0,0 +1,106 @@
+# Weather App
+
+## Дипломный проект по курсу "Android разработчик с нуля" - Приложение "Погода".
+
+Это многоэкранное приложение с большим набором получаемых данных и развитым функционалом!
+
+ #### Реализованный функционал:
+
+- получение более детальной информации с погодой на текущий момент
+- получение погодных данных на 2 дня вперед
+- получение данных астрономического характера
+- получение почасового прогноза погоды на 2 дня вперед
+- получение местоположения пользователя
+- ограничение в отображении данных неавторизованного пользователя
+- регистрация/авторизация пользователя через Firebase
+- смена языка "состояния погоды"
+- горизонтальная ориентация экрана
+- всевозможные подсказки и предупреждения для пользователя:
+  - Нет разрешения на получение местоположения
+  - Не включин GPS
+  - некорректный ввод названия города
+  - некорректный формат ввода электронной почты
+  - слишком короткий пароль
+
+
+#### Инструменты и технологии реализации:
+
+- Single Activity
+- архитектура mvvm
+- библиотека volley
+- LiveData
+- библиотека picasso
+- Firebase Authentication
+- Coroutines
+- Hilt
+- JUnit4 tests
+
+#### Наглядная демонстрация функционала:
+
+_Запрос города на кирилице и латинице_
+
+<image src="/ScreenShots/5.jpg" width=30% height=30%>
+<image src="/ScreenShots/7.jpg" width=30% height=30%>
+
+_Подробная информация о погоде_
+
+<image src="/ScreenShots/7.jpg" width=30% height=30%>
+
+_Запрос о получении местоположения и предупреждения об ограничениях_
+
+<image src="/ScreenShots/1.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/2.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/3.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/22.jpg" width=30% height=30%>
+
+_ограничение в отображении получаемых даннх для неавторизованного пользователя_
+
+<image src="/ScreenShots/8.jpg" width=30% height=30%>
+
+_регистрация и авторизация_
+
+<image src="/ScreenShots/9.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/11.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/12.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/13.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/14.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/15.jpg" width=30% height=30%>
+
+_Экран с астронамическими данными_
+
+<image src="/ScreenShots/16.jpg" width=30% height=30%>
+
+_Экран с прогнозом на 2 дня вперед_
+
+<image src="/ScreenShots/17.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/18.jpg" width=30% height=30%>
+
+_Экран настроек_
+
+<image src="/ScreenShots/10.jpg" width=30% height=30%>
+
+_некорректный ввод имени города_
+
+<image src="/ScreenShots/20.jpg" width=30% height=30%>
+
+<image src="/ScreenShots/21.jpg" width=30% height=30%>
+
+_пример использования итальянского языка для отображения "состояния погоды"_
+
+<image src="/ScreenShots/19.jpg" width=30% height=30%>
+
+_горизонтальная ориентация экрана_
+
+<image src="/ScreenShots/23.jpg" width=30% height=30%>
+
+
+#### APK-файл находится в папке apk
--- app/src/test/java/ru/nikita/weatherdiplom/utils/MoonPhasesTest1.kt
+++ app/src/test/java/ru/nikita/weatherdiplom/utils/MoonPhasesTest1.kt
@@ -1,115 +1,118 @@
 package ru.nikita.weatherdiplom.utils
 
 import org.junit.Assert
+
 import org.junit.Test
 
-
 class MoonPhasesTest {
+
     @Test
     fun changeMoonPhasesExpectedNewMoon() {
-        val cameWord = MoonPhases.changeMoonPhases("New Moon")
+        val cameWord = MoonPhases().changeMoonPhases("New Moon")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.new_moon
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
+
     @Test
     fun changeMoonPhasesExpectedWaxingCrescent() {
-        val cameWord = MoonPhases.changeMoonPhases("Waxing Crescent")
+        val cameWord = MoonPhases().changeMoonPhases("Waxing Crescent")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.waxing_crescent
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
+
     @Test
     fun changeMoonPhasesExpectedFirstQuarter() {
-        val cameWord = MoonPhases.changeMoonPhases("First Quarter")
+        val cameWord = MoonPhases().changeMoonPhases("First Quarter")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.first_quarter
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun changeMoonPhasesExpectedWaxingGibbous() {
-        val cameWord = MoonPhases.changeMoonPhases("Waxing Gibbous")
+        val cameWord = MoonPhases().changeMoonPhases("Waxing Gibbous")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.waxing_gibbous
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun changeMoonPhasesExpectedFullMoon() {
-        val cameWord = MoonPhases.changeMoonPhases("Full Moon")
+        val cameWord = MoonPhases().changeMoonPhases("Full Moon")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.full_moon
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun changeMoonPhasesExpectedWaningGibbous() {
-        val cameWord = MoonPhases.changeMoonPhases("Waning Gibbous")
+        val cameWord = MoonPhases().changeMoonPhases("Waning Gibbous")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.waning_gibbous
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun changeMoonPhasesExpectedLastQuarter() {
-        val cameWord = MoonPhases.changeMoonPhases("Last Quarter")
+        val cameWord = MoonPhases().changeMoonPhases("Last Quarter")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.last_quarter
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun changeMoonPhasesExpectedWaningCrescent() {
-        val cameWord = MoonPhases.changeMoonPhases("Waning Crescent")
+        val cameWord = MoonPhases().changeMoonPhases("Waning Crescent")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.waning_crescent
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun changeMoonPhasesExpectedElse() {
-        val cameWord = MoonPhases.changeMoonPhases("test")
+        val cameWord = MoonPhases().changeMoonPhases("test")
         val moonPhaseFromString = ru.nikita.weatherdiplom.R.string.error_moon_phase
         Assert.assertEquals(cameWord, moonPhaseFromString)
     }
     @Test
     fun setMoonImageExpectedNewMoon() {
-        val cameWord = MoonPhases.setMoonImage("New Moon")
+        val cameWord = MoonPhases().setMoonImage("New Moon")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.new_moon
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedWaxingCrescent() {
-        val cameWord = MoonPhases.setMoonImage("Waxing Crescent")
+        val cameWord = MoonPhases().setMoonImage("Waxing Crescent")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.waxing_crescent
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedFirstQuarter() {
-        val cameWord = MoonPhases.setMoonImage("First Quarter")
+        val cameWord = MoonPhases().setMoonImage("First Quarter")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.first_quarter
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedWaxingGibbous() {
-        val cameWord = MoonPhases.setMoonImage("Waxing Gibbous")
+        val cameWord = MoonPhases().setMoonImage("Waxing Gibbous")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.waxing_gibbous
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedFullMoon() {
-        val cameWord = MoonPhases.setMoonImage("Full Moon")
+        val cameWord = MoonPhases().setMoonImage("Full Moon")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.full_moon
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedWaningGibbous() {
-        val cameWord = MoonPhases.setMoonImage("Waning Gibbous")
+        val cameWord = MoonPhases().setMoonImage("Waning Gibbous")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.waning_gibbous
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedLastQuarter() {
-        val cameWord = MoonPhases.setMoonImage("Last Quarter")
+        val cameWord = MoonPhases().setMoonImage("Last Quarter")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.last_quarter
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedWaningCrescent() {
-        val cameWord = MoonPhases.setMoonImage("Waning Crescent")
+        val cameWord = MoonPhases().setMoonImage("Waning Crescent")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.waning_crescent
         Assert.assertEquals(cameWord, moonImageFromString)
     }
     @Test
     fun setMoonImageExpectedElse() {
-        val cameWord = MoonPhases.setMoonImage("test")
+        val cameWord = MoonPhases().setMoonImage("test")
         val moonImageFromString = ru.nikita.weatherdiplom.R.drawable.ic_error_24
         Assert.assertEquals(cameWord, moonImageFromString)
     }
